05aeb6311b9ae5f978ee9a96efa4d7d7f5f67e00
Zainul Abideen <zainulabd786@gmail.com>
2018-12-04 10:31:52 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,23 @@
+function acf_load_menu_field_choices( $field ) {
+    
+    // reset choices
+    $field['choices'] = array();
+
+    $menus = get_terms( 'nav_menu', array( 'hide_empty' => true ) );
+    //$menus = get_registered_nav_menus();
+    $blank_list = json_encode(array( "name" => "Default Menu", "term_id" => "")); 
+    $blank_list = json_decode($blank_list);
+    array_unshift($menus, $blank_list);
+	foreach ( $menus as $val ) {
+
+		$value = $val->term_id;
+        $label = $val->name;
+
+        $field['choices'][ $value ] = $label;
+	}
+
+    // return the field
+    return $field;
+    
+}
+add_filter('acf/load_field/name=custom_menu', 'acf_load_menu_field_choices');  //replace custom_menu with your field name
